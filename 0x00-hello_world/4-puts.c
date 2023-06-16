7070c607b65e596bb467ea33b84bc1604ec63245
Nolity <jnolity232@gmail.com>
2023-06-16 12:34:09 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio .h>
 /**
- * main - Print a line with puts function
- * Return: 0 (Accomplished)
+ * main - Program that prints a line with puts function
+ * Return: 0
  */
 int main(void)
 {
