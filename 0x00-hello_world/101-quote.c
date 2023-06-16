d0856d916b870334a4f1e38cc306edddc5826859
Nolity <jnolity232@gmail.com>
2023-06-16 14:52:26 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main - Program that prints a lineto a standard error
- * Return: 0
+ * Return: 1
  */
 int main(void)
 {
 fprintf(stderr, "\"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-return 0;
+return (1);
 }
