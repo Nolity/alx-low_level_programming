3b3bb75abc6c3938990e3c8d5b28f740dc33172a
Nolity <jnolity232@gmail.com>
2023-06-16 13:43:37 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,9 +11,9 @@ long int c;
 long long int d;
 float e;
 
-printf("Size of a char size= %zu", sizeof(a));
-printf("\nSize of a int size= %zu", sizeof(b));
-printf("\nSize of a long int size= %zu", sizeof(c));
+printf("Size of a char= %zu", sizeof(a));
+printf("\nSize of a int= %zu", sizeof(b));
+printf("\nSize of a long int= %zu", sizeof(c));
 printf("\nSize of a long long int= %zu", sizeof(d));
 printf("\nSize of a float= %zu", sizeof(e));
 
