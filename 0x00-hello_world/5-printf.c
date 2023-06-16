7539ff774f296e432695c5ea94e4133b405720a3
Nolity <jnolity232@gmail.com>
2023-06-16 13:03:54 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,8 +3,9 @@
  * main - Program that prints a line using the printf function
  * Return: 0
  */
+
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,");
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+	return (0);
 }
