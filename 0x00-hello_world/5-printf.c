90c1f4105f22e326ce1bb7d5a012fd7eba571641
Nolity <jnolity232@gmail.com>
2023-06-16 12:52:54 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,10 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+ * main - Program that prints a line using the printf function
+ * Return: 0
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
