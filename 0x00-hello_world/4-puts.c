2a3695f8c80ad2c888e268891da071e5f6fad90f
Nolity <jnolity232@gmail.com>
2023-06-16 12:30:45 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,11 @@
-#!/bin/bash
+#include <stdio .h>
+/**
+ * main - Print a line with puts function
+ * Return: 0 (Accomplished)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+
