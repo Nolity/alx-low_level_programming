82cb10cf8a081672e651f74bd6988b0163a82196
Nolity <jnolity232@gmail.com>
2023-06-17 12:56:25 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - A program that will print all single digit numbers
+ * Return: 0 (Successful)
+ */
+int main(void)
+{
+int n;
+for (n <= 0; n < 10; n++)
+{/*if condition is true print*/
+	printf("%d", n);
+}
+printf("\n");
+return (0);
+}
