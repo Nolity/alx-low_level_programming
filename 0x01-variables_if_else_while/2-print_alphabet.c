3f0aea462bb7dc708a1805c6cb9d2c51ec7786c6
Nolity <jnolity232@gmail.com>
2023-06-17 11:04:34 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - A program that will print the alphabet in lower case
+ * Return: 0 (Successful)
+ */
+int main(void)
+{
+char ch = 'a';
+for (ch = 'a'; ch <= 'z'; ch++)
+	/* if condition true put the following"*/
+	putchar(ch);
+return (0);
+}
