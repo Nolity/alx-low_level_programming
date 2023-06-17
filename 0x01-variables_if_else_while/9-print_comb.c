e3af1fccb1353441f9d8e72668a0ebb92fede396
Nolity <jnolity232@gmail.com>
2023-06-17 14:32:45 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - A program that will put all the numbers of base16
+ * Return: 0 (Successful)
+ */
+int main(void)
+{
+	int n;
+for (n = 0; n < 10; n++)
+{
+/*if condition is true put the following*/
+	putchar(a + '0');
+if (n < 9)
+{
+/*if condition is true put the following*/
+	putchar(',');
+	putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
