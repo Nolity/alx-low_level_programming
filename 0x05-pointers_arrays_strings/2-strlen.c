709189976431b372c6eb71ffc12cabf2caa59c9c
Nolity <jnolity232@gmail.com>
2023-06-27 01:53:17 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * strlen - a function that returns the length of a string.
- * @s : string
+ * @s : the string to calculate the length of.
  * Return : length
  */
 int _strlen(char *s)
