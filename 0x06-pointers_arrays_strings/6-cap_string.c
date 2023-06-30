84c7dbd512462af92efb1fb97c9370cd0622f2f3
Nolity <jnolity232@gmail.com>
2023-06-30 11:28:58 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,23 +1,42 @@
 #include "main.h"
 
 /**
- * string_toupper - Changes all lowercase letters of a string to uppercase.
+ * cap_string - Capitalizes all words of a string.
  * @str: Pointer to the string.
  *
  * Return: Pointer to the resulting string.
  */
-char *string_toupper(char *str)
+char *cap_string(char *str)
 {
 	int i = 0;
 
+	/* Flag to indicate the next character should be capitalized*/
+	int capitalize_next = 1;
+
 	while (str[i] != '\0')
 	{
-		if (str[i] >= 'a' && str[i] <= 'z')
+		/* Check if the current character is a separator*/
+		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
+				str[i] == ',' || str[i] == ';' || str[i] == '.' ||
+				str[i] == '!' || str[i] == '?' || str[i] == '"' ||
+				str[i] == '(' || str[i] == ')' || str[i] == '{' ||
+				str[i] == '}')
+		{
+			capitalize_next = 1;
+		}
+
+		/* Check if the current character is a lowercase letter*/
+		else if (str[i] >= 'a' && str[i] <= 'z' && capitalize_next)
 		{
 			str[i] = str[i] - 32; /* Convert lowercase to uppercase*/
+			capitalize_next = 0;
 		}
+		else
+		{
+			capitalize_next = 0;
+		}
+
 		i++;
 	}
-
 	return (str);
 }
