b09f17763ac37cd017dbd73ed0b7bea2493a1dd2
Nolity <jnolity232@gmail.com>
2023-07-06 11:18:44 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,24 +1,6 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion - Returns the natural square root of a number.
- * @n: Number to find the square root of
- *
- * Return: The square root of n, or -1 if n does not have a natural square root
- */
-int _sqrt_recursion(int n)
-{
-	if (n < 0)
-		/* Return -1 if n is negative */
-		return (-1);
-	if (n == 0 || n == 1)
-		/* Return n if n is 0 or 1 */
-		return (n);
-
-	/*Call the helper function*/
-	return (sqrt_helper(n, 1, n / 2));
-}
-/**
  * sqrt_helper - Helper function to calculate the square root recursively
  * @n: The number to find the square root of
  * @start: The starting point for the search
@@ -26,30 +8,41 @@ int _sqrt_recursion(int n)
  *
  * Return: The square root of n, or -1 if n does not have a natural square root
  */
-
 int sqrt_helper(int n, int start, int end)
 {
-	int mid, square;
+	if (start > end)
+		return (-1);
+	/* Return -1 if n does not have a natural square root */
 
-	if (start <= end)
-	{
-		mid = (start + end) / 2;
-		square = mid * mid;
+	int mid = (start + end) / 2;
+	int square = mid * mid;
 
-		if (square == n)
-			/* Return mid if mid^2 equals n */
-			return (mid);
+	if (square == n)
+		return (mid);
+	/* Return mid if mid^2 equals n */
 
-		else if (square < n)
+	else if (square > n)
+		return (sqrt_helper(n, start, mid - 1));
+	/* Search the lower half */
 
-			/* Search the upper half */
-			return (sqrt_helper(n, mid + 1, end));
+	else
+		return (sqrt_helper(n, mid + 1, end));
+	/* Search the upper half */
+}
 
-		else
-			/* Search the lower half */
-			return (sqrt_helper(n, start, mid - 1));
-	}
+/**
+ * _sqrt_recursion - Calculates the natural square root of a number
+ * @n: The number to find the square root of
+ *
+ * Return: The square root of n, or -1 if n does not have a natural square root
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1)
+			/* Return -1 if n is negative */
 
-	/* Return the previous value of end as the square root */
-	return (end);
+			return (sqrt_helper(n, 0, n));
+	/* Call the helper function */
+}
 }
