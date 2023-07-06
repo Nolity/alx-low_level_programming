44112303ad3af84fafbb88a9843f1e3758cd346e
Nolity <jnolity232@gmail.com>
2023-07-06 11:06:37 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -18,3 +18,38 @@ int _sqrt_recursion(int n)
 	/*Call the helper function*/
 	return (sqrt_helper(n, 1, n / 2));
 }
+/**
+ * sqrt_helper - Helper function to calculate the square root recursively
+ * @n: The number to find the square root of
+ * @start: The starting point for the search
+ * @end: The ending point for the search
+ *
+ * Return: The square root of n, or -1 if n does not have a natural square root
+ */
+
+int sqrt_helper(int n, int start, int end)
+{
+	int mid, square;
+
+	if (start <= end)
+	{
+		mid = (start + end) / 2;
+		square = mid * mid;
+
+		if (square == n)
+			/* Return mid if mid^2 equals n */
+			return (mid);
+
+		else if (square < n)
+
+			/* Search the upper half */
+			return (sqrt_helper(n, mid + 1, end));
+
+		else
+			/* Search the lower half */
+			return (sqrt_helper(n, start, mid - 1));
+	}
+
+	/* Return the previous value of end as the square root */
+	return (end);
+}
